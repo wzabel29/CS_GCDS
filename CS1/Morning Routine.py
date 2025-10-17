f018461ab64c61165744440b276752addbd333cb
wzabel29 <wzabel29@gcds.net>
2025-10-17 10:17:54 -0400

    Update Morning Routine.py

--- CS1/Morning Routine.py
+++ CS1/Morning Routine.py
@@ -6,3 +6,11 @@ while True:
     else:
         print("wake up!")
         break
+while True:
+    guess - input("Yes or no, Look at phone?").lower()
+    if guess == "yes":
+        print("stay in bed")
+    elif "stay in bed":
+        print('get out of bed')
+    else:
+        print('get out of bed')
