7adafabe563bf310beb28e82b6ef5a3bde657142
wzabel29 <wzabel29@gcds.net>
2025-10-18 21:02:43 -0400

    Update Untitled-2.py

--- CS1/Untitled-2.py
+++ CS1/Untitled-2.py
@@ -1,10 +1,2 @@
-color = "red"
-
-while True:
-    guess = input('Enter your color guess: ').lower()
-
-    if color == guess:
-        print('you got it')
-        break
-    else:
-        print('try again')
+x = 2.2729373993739373937338373837202416282
+x * y = 20
