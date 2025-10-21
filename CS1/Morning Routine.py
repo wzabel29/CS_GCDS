956337b2b1145c648c36f573bdda20e63e467909
wzabel29 <wzabel29@gcds.net>
2025-10-21 14:56:34 -0400

    Update Morning Routine.py

--- CS1/Morning Routine.py
+++ CS1/Morning Routine.py
@@ -1,15 +1,23 @@
+import time
 print ("Alarm!")
+
 while True:
-    guess = input("Yes or No, Would you like to snooze?").lower()
-    if guess == "yes":
+    snooze = input("Yes or No, Would you like to snooze?").lower()
+
+    if snooze == "yes":
         print("Go back to sleep")
-    else:
+    elif snooze == "no":
         print("wake up!")
         break
+    else:
+        print("Please enter yes or no")
+
 while True:
-    guess = input("Yes or no, Look at phone?").lower()
-    if guess == "yes":
-        print("stay in bed")
+    phone = input("Yes or no, Look at phone?").lower()
+
+    if phone == "yes":
+        print("stay in bed for 15 min")
+        time.sleep(2)
         print('get out of bed')
         break
     else:
@@ -17,6 +25,7 @@ while True:
         break
 while True:
     guess = input("Yes or no, go downstairs?").lower()
+
     if guess == "yes":
         print("Eat Breakfast")
         break
@@ -25,11 +34,12 @@ while True:
         print("Eat Breakfast")
         break
 while True:
-    guess = input("Waffles or Pancakes?")
+    guess = input("Waffles or Pancakes?").lower()
+    
     if guess == "Waffles":
         print('Eat Waffles')
         break
-    else:
+    elif guess == "Pancakes":
         print('Eat Pancakes')
         break
 print('Shower')
@@ -41,6 +51,14 @@ while True:
         break
     else:
         print("wear shorts")
+        break
+while True:
+    guess = input("Yes or no, Is it cold enough to wear a sweatshirt?").lower()
+    if guess == "yes":
+        print("Wear sweatshirt")
+        break
+    else:
+        print("Don't wear a sweatshirt")
         break
 print("pack back")
 print("drive to school")
