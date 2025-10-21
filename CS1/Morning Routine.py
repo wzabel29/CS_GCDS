c2bcaf746f3a0f1aa0d2e6a5413aec1ed9f9d9e7
wzabel29 <wzabel29@gcds.net>
2025-10-21 14:43:18 -0400

    Update Morning Routine.py

--- CS1/Morning Routine.py
+++ CS1/Morning Routine.py
@@ -10,6 +10,8 @@ while True:
     guess = input("Yes or no, Look at phone?").lower()
     if guess == "yes":
         print("stay in bed")
+        print('get out of bed')
+        break
     else:
         print('get out of bed')
         break
@@ -20,3 +22,26 @@ while True:
         break
     else:
         print("Go to Bathroom")
+        print("Eat Breakfast")
+        break
+while True:
+    guess = input("Waffles or Pancakes?")
+    if guess == "Waffles":
+        print('Eat Waffles')
+        break
+    else:
+        print('Eat Pancakes')
+        break
+print('Shower')
+print('Get dressed')
+while True:
+    guess = input("Yes or no, Is it cold outside").lower()
+    if guess == "yes":
+        print("wear pants")
+        break
+    else:
+        print("wear shorts")
+        break
+print("pack back")
+print("drive to school")
+print("Arrive at school")
