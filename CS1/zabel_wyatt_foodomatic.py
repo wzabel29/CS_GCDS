1f95dc0c5ea02ae3e76e304efd7d78dcdfd8fd8e
wzabel29 <wzabel29@gcds.net>
2026-03-29 19:27:23 -0400

    Update zabel_wyatt_foodomatic.py

--- CS1/zabel_wyatt_foodomatic.py
+++ CS1/zabel_wyatt_foodomatic.py
@@ -1,9 +1,9 @@
 import random
 
-mains = ["Atlantic salmon served on a bed of wild rice","Burger with Truffle Fries ","Halibut Piccata", "Chicken Picatta","potatoe Gnocci","three color squash Rissoto", "eggplant parm", "Tomahawk steak","escargo with baguette"]
-prices = int["20", "25", "28", "30", "18", "20", "22", "30", "20"]
-flairs = ["with balsamico","with garlic and olive oil","with minted yogurt","with chutney","with a salad", "with salsa", "over sticky rice","au jus", "with basmati rice"]
-flair_prices = int["3", "4", "5", "5", "7", "4", "4", "6", "5"]
+mains = ["Spicy salmon roll","Chicken fried rice","Pork lo mien", "Spicy tonkutsu ramen","Chicken and brocoli","Crab soup dumplings", "Pan fried pork dumplings", "Tuna and avocado roll","Shrimp tempura roll"]
+main_prices = ["20", "25", "28", "30", "18", "20", "22", "30", "20"]
+flairs = ["with spicy edemamme","with soy sauce and spicy mayo","with crab rangoons","with white rice","with seaweed salad", "with miso soup", "with vegetable springrolls","with spicy cucumbers", "with egg drop soup"]
+flair_prices = ["3", "4", "5", "5", "7", "4", "4", "6", "5"]
 user_items = int(input('How many items do you need?'))
 
 for i in range(user_items):
@@ -11,9 +11,10 @@ for i in range(user_items):
     flair = random.choice(flairs)
     index_main = mains.index(main)
     index_flair = flairs.index(flair)
-    final_price = flair_prices[index_flair] + prices[index_main]
+    final_price = int(flair_prices[index_flair]) + int(main_prices[index_main])
 
-    print(f"{main} is ${prices[index_main]} with {flair} which costs ${flair_prices[index_flair]}. The total price for your meal is ${final_price}.")
+    print(f"{main} is ${main_prices[index_main]} {flair} which costs ${flair_prices[index_flair]}. The total price for your meal is ${final_price}.")
 
-# print(f"The final price for your meal is ${Meal_Price}")
+meal_price = sum(int(flair_prices[mains.index(main)]) + int(main_prices[mains.index(main)]) for main in random.choices(mains, k=user_items))
+print(f"The final price for your meal is ${meal_price}")
 
